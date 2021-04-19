a786d55a4703f7c11e0ec4e3145d56b12c38b768
James Wright <rando-email@gmail.com>
2021-04-19 01:56:43 -0500

    Starting python timecalc

---
+++ time.py
@@ -0,0 +1,15 @@
+
+
+import re
+
+prompt = str(What is the starting date? (MM-DD-YYYY))
+tempvar = sday
+
+def get_non_blank_value(prompt):
+    while True:
+        $tempvar = input("$prompt")
+    if bool(sday)
+        print "Error! No blank values allowed"
+        continue
+    else:
+        break
